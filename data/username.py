cf81c6f98e2730ce8f98723f4f2984e4782881a2
Dramatuc6486 <131907553+Dramatuc6486@users.noreply.github.com>
2024-10-06 17:24:13 -0700

    Update username.py

--- data/username.py
+++ data/username.py
@@ -1,1 +1,8 @@
+for i in open("wl.txt","r",encoding="utf8").read().splitlines():
+    name = i.split(":")[0]
+
+    if (len(name) > 7) and (len(name) < 20):
+        if not (" " in name) and not ("." in name) and not ("_" in name):
+            open("nl.txt","a").write(name + "\n")
+
 
